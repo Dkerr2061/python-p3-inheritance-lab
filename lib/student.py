6f3643e315e41d1f8faf469ebc6bc2df8a24b14f
David Kerr <kerrky@gmail.com>
2024-03-15 12:13:57 -0400

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+
+    knowledge = []
+
+    def __init__(self, first_name, last_name, knowledge= knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge):
+        if(type(new_knowledge) is str):
+            self.knowledge.append(new_knowledge)
+        
